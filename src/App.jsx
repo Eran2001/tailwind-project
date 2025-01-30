0a2b663af7911af2dd2ceb83f11b4be15134a373
Eran <eran.hasarali.2001@gmail.com>
2025-01-30 18:48:26 +0530

    install tailwind and add some utility classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="flex justify-center items-center h-screen bg-gray-300">
+      <h1 className="text-purple-600 text-3xl font-bold underline sm:text-red-600 md:text-blue-600 lg:text-green-400 xl:text-orange-500">
+        Hello
+      </h1>
     </div>
   );
 };
